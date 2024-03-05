6c877feeaedd274e0ac431ad79a3a91e4da633c0
Alexis Lhussiez <alexis.lhu@gmail.com>
2024-03-05 16:13:05 +0100

    import du jeu

---
+++ memorygame/src/App.js
@@ -0,0 +1,197 @@
+import { useEffect, useState, useRef } from "react";
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  Button,
+  DialogTitle
+} from "@material-ui/core";
+import Card from "./components/card.js";
+import "./App.css";
+
+const uniqueElementsArray = [
+  {
+    type: "Telo",
+    image: require(`./images/telo.jpg`)
+  },
+  {
+    type: "Alexis",
+    image: require(`./images/alexis.jpeg`)
+  },
+  {
+    type: "Teo",
+    image: require(`./images/teo.jpg`)
+  },
+  {
+    type: "Hugo",
+    image: require(`./images/hugo.png`)
+  },
+  {
+    type: "Theo",
+    image: require(`./images/theo.jpg`)
+  },
+  {
+    type: "theo2",
+    image: require(`./images/theo2.jpg`)
+  }
+];
+
+function shuffleCards(array) {
+  const length = array.length;
+  for (let i = length; i > 0; i--) {
+    const randomIndex = Math.floor(Math.random() * i);
+    const currentIndex = i - 1;
+    const temp = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temp;
+  }
+  return array;
+}
+export default function App() {
+  const [cards, setCards] = useState(
+    shuffleCards.bind(null, uniqueElementsArray.concat(uniqueElementsArray))
+  );
+  const [openCards, setOpenCards] = useState([]);
+  const [clearedCards, setClearedCards] = useState({});
+  const [shouldDisableAllCards, setShouldDisableAllCards] = useState(false);
+  const [moves, setMoves] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [bestScore, setBestScore] = useState(
+    JSON.parse(localStorage.getItem("bestScore")) || Number.POSITIVE_INFINITY
+  );
+  const timeout = useRef(null);
+
+  const disable = () => {
+    setShouldDisableAllCards(true);
+  };
+  const enable = () => {
+    setShouldDisableAllCards(false);
+  };
+
+  const checkCompletion = () => {
+    if (Object.keys(clearedCards).length === uniqueElementsArray.length) {
+      setShowModal(true);
+      const highScore = Math.min(moves, bestScore);
+      setBestScore(highScore);
+      localStorage.setItem("bestScore", highScore);
+    }
+  };
+  const evaluate = () => {
+    const [first, second] = openCards;
+    enable();
+    if (cards[first].type === cards[second].type) {
+      setClearedCards((prev) => ({ ...prev, [cards[first].type]: true }));
+      setOpenCards([]);
+      return;
+    }
+    timeout.current = setTimeout(() => {
+      setOpenCards([]);
+    }, 500);
+  };
+  const handleCardClick = (index) => {
+    if (openCards.length === 1) {
+      setOpenCards((prev) => [...prev, index]);
+      setMoves((moves) => moves + 1);
+      disable();
+    } else {
+      clearTimeout(timeout.current);
+      setOpenCards([index]);
+    }
+  };
+
+  useEffect(() => {
+    let timeout = null;
+    if (openCards.length === 2) {
+      timeout = setTimeout(evaluate, 300);
+    }
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [openCards]);
+
+  useEffect(() => {
+    checkCompletion();
+  }, [clearedCards]);
+  const checkIsFlipped = (index) => {
+    return openCards.includes(index);
+  };
+
+  const checkIsInactive = (card) => {
+    return Boolean(clearedCards[card.type]);
+  };
+
+  const handleRestart = () => {
+    setClearedCards({});
+    setOpenCards([]);
+    setShowModal(false);
+    setMoves(0);
+    setShouldDisableAllCards(false);
+    setCards(shuffleCards(uniqueElementsArray.concat(uniqueElementsArray)));
+  };
+
+  return (
+    <div className="App">
+      <header>
+        <p>Flip Flap la classe</p>
+        <div id="texte">
+        Sélectionnez consécutivement deux cartes avec le même contenu pour les faire disparaître !
+        </div>
+      </header>
+      <div className="container">
+        {cards.map((card, index) => {
+          return (
+            <Card
+              key={index}
+              card={card}
+              index={index}
+              isDisabled={shouldDisableAllCards}
+              isInactive={checkIsInactive(card)}
+              isFlipped={checkIsFlipped(index)}
+              onClick={handleCardClick}
+            />
+          );
+        })}
+      </div>
+      <footer>
+        <div className="score">
+          <div className="moves">
+            <span className="bold">Mouvements:</span> {moves}
+          </div>
+          {localStorage.getItem("bestScore") && (
+            <div className="high-score">
+              <span className="bold">Record:</span> {bestScore}
+            </div>
+          )}
+        </div>
+        <div className="restart">
+          <Button onClick={handleRestart} color="primary" variant="contained">
+            Try again
+          </Button>
+        </div>
+      </footer>
+      <Dialog
+        open={showModal}
+        disableBackdropClick
+        disableEscapeKeyDown
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          GG Zeynel t'es trop le boss du game !!
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Vous avez réussi en seulement {moves} mouvements !! Votre record est en{" "}
+            {bestScore} mouvements.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleRestart} color="primary">
+            Try again
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
